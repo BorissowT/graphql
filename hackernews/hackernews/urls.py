d7f3d759db22fa11ed2d59ba18f6be5450d9f866
tim <timsregisters@gmail.com>
2022-11-11 17:31:03 +0100

    finish authorisation

--- hackernews/hackernews/urls.py
+++ hackernews/hackernews/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('', RedirectView.as_view(url='graphql/')),
 ]
